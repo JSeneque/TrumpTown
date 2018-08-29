91cc2ff1cde3df69a90507c0af0fa812f4c9ddd1
Jean-Noel Seneque <jeannoelseneque@hotmail.com>
2018-08-29 18:15:41 +0930

    added jacob work

---
+++ TrumpTown/Assets/Scripts/Splash.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Splash : MonoBehaviour {
+
+    public Image splashBG; // Splash Background
+    public string loadMainMenu; // Move to next scene
+
+    IEnumerator Start()
+    {
+        splashBG.canvasRenderer.SetAlpha(0.0f);
+
+        FadeIn();
+        yield return new WaitForSeconds(2.5f);
+        FadeOut();
+        yield return new WaitForSeconds(2.5f);
+
+        SceneManager.LoadScene(loadMainMenu);
+    }
+
+    void FadeIn()
+    {
+        splashBG.CrossFadeAlpha(1.0f, 1.5f, false);
+    }
+
+    void FadeOut()
+    {
+        splashBG.CrossFadeAlpha(0.0f, 2.5f, false);
+    }
+
+    
+}
